0d9919057b03ba0a50e3588140274a2e8017a9c0
LinhTran1209 <143068570+LinhTran1209@users.noreply.github.com>
2026-07-20 16:18:26 +0700

    feat: publish settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+Exit code: 0
+Wall time: 0.6 seconds
+Output:
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "File Server"
+include(":app")
+
+
