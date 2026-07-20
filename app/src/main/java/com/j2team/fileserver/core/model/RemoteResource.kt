6b268c0bb9d392e9afcf3b17d53ff811ecc45afc
LinhTran1209 <143068570+LinhTran1209@users.noreply.github.com>
2026-07-20 16:19:51 +0700

    feat: publish app/src/main/java/com/j2team/fileserver/core/model/RemoteResource.kt

---
+++ app/src/main/java/com/j2team/fileserver/core/model/RemoteResource.kt
@@ -0,0 +1,12 @@
+Exit code: 0
+Wall time: 0.6 seconds
+Output:
+package com.j2team.fileserver.core.model
+
+data class RemoteResource(
+    val name: String,
+    val path: String,
+    val isDirectory: Boolean,
+    val size: Long,
+)
+
