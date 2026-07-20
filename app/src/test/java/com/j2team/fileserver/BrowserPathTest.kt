c4a710bb40828c699d3255138a471850a20dacf5
LinhTran1209 <143068570+LinhTran1209@users.noreply.github.com>
2026-07-20 17:27:50 +0700

    Rebuild app from Figma and wire full flows

---
+++ app/src/test/java/com/j2team/fileserver/BrowserPathTest.kt
@@ -0,0 +1,24 @@
+Exit code: 0
+Wall time: 0.6 seconds
+Output:
+package com.j2team.fileserver
+
+import com.j2team.fileserver.feature.browser.BrowserPath
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class BrowserPathTest {
+    @Test
+    fun parentNeverEscapesRoot() {
+        assertEquals("/", BrowserPath.parent("/"))
+        assertEquals("/", BrowserPath.parent("/home"))
+        assertEquals("/home", BrowserPath.parent("/home/linh3"))
+    }
+
+    @Test
+    fun childEncodesNoNavigationSegments() {
+        assertEquals("/home/linh3", BrowserPath.child("/home", "linh3"))
+        assertEquals("/home", BrowserPath.child("/", "home"))
+    }
+}
+
