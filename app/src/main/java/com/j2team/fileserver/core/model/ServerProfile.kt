4ec97132de49c632cfcfeb60fed68a701a178654
LinhTran1209 <143068570+LinhTran1209@users.noreply.github.com>
2026-07-20 16:19:46 +0700

    feat: publish app/src/main/java/com/j2team/fileserver/core/model/ServerProfile.kt

---
+++ app/src/main/java/com/j2team/fileserver/core/model/ServerProfile.kt
@@ -0,0 +1,16 @@
+Exit code: 0
+Wall time: 0.6 seconds
+Output:
+package com.j2team.fileserver.core.model
+
+data class ServerProfile(
+    val id: String,
+    val displayName: String,
+    val scheme: String,
+    val host: String,
+    val port: Int,
+    val basePath: String = "/",
+) {
+    val endpoint: String get() = "$scheme://$host:$port${basePath.trimEnd('/')}/"
+}
+
